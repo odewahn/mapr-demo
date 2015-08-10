5740dd7e32ce7608f3e6087dd44333cab1e356eb
Andrew Odewahn <odewahn@oreilly.com>
2015-08-10 10:18:21 -0400

    adding twitter example

--- start.sh
+++ start.sh
@@ -7,15 +7,15 @@
 #
 #  Need to figure out if there's a real way to daemonize it
 #
+#!/bin/bash
 
 export ODBCINI=/usr/home/.odbc.ini
 export MAPRDRILLINI=/usr/home/.mapr.drillodbc.ini
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mapr/drillodbc/lib/64:/usr/lib
 
-# /opt/drill/apache-drill-1.1.0/bin/drill-embedded &
-
 echo "Starting local embedded Drillbit"
-screen -d -m /opt/drill/apache-drill-1.1.0/bin/drill-embedded
+screen -S pytest -d -m /opt/drill/apache-drill-1.1.0/bin/drill-embedded
+sleep 20
 
 echo "Starting Jupyter Notebook"
 
@@ -25,3 +25,8 @@ ipython2 notebook \
    --port 8888 \
    --ip=* \
    --NotebookApp.allow_origin=*
+
+# send a ctrl-D
+echo "Shutting down Drillbit"
+screen -S pytest -X stuff `echo -ne '\004'`
+sleep 5
